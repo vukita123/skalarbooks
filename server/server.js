b2c744f9775f7d537e1d3c8a210e20b8dc30b525
Luka Vrbaski <lukavrbaski@Lukas-MacBook-Pro.local>
2022-02-23 13:02:49 +0100

    ss

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ const path = require("path");
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
+console.log(__dirname);
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
